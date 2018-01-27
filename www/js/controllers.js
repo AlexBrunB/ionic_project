79a866f24f46a1cfd6242f7c22d849487a5054d5
fremon_r <fremont.raphael@gmail.com>
2018-01-27 15:40:41 +0100

    Account page, edit page INC

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,5 +1,5 @@
 angular.module('starter.controllers', [])
-.controller('LoginCtrl', function($scope, $state, BackendAPI, $ionicLoading, $ionicPopup) {
+.controller('LoginCtrl', function($scope, $rootScope, $state, BackendAPI, $ionicLoading, $ionicPopup) {
     $scope.loginUser = { login: null, password: null };
     $scope.goToSignup = function(){
     $state.go('signup');
@@ -8,10 +8,10 @@ angular.module('starter.controllers', [])
     $ionicLoading.show({
       template: 'Loading...'
         });
-        console.log('user : ' + $scope.loginUser);
         BackendAPI.login($scope.loginUser)
         .then(function(res){
           $ionicLoading.hide();
+          $rootScope.currUser = res.data;
           $state.go('tab.dash');
         })
         .catch(function(err) {
@@ -69,10 +69,18 @@ console.log("Finish ");
   $scope.chat = Chats.get($stateParams.chatId);
 })
 
-.controller('AccountCtrl', function($scope, $state, BackendAPI) {
-  $scope.currUser = BackendAPI;
+.controller('AccountCtrl', function($scope, $rootScope, $state, BackendAPI) {
+  $scope.profilEdit = {status:false};
+
 
   $scope.logout = function() {
+    $scope.currUser = null;
     $state.go('login');
-  }
+  };
+
+  $scope.editclick = function() {
+    console.log('prev: ' + $scope.profilEdit.status);
+    $scope.profilEdit.status = !$scope.profilEdit.status;
+        console.log('next: ' + $scope.profilEdit.status);
+  };
 });
