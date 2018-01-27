a9c56b3210a28ed11c7f907e80179eea84faf2e6
abrun <alexandre.brun@epitech.eu>
2018-01-27 15:50:47 +0100

    Add Final Page

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -60,6 +60,7 @@ console.log("Finish ");
 
 })
 
+
 .controller('ChatsCtrl', function($scope, Chats) {
   // With the new view caching in Ionic, Controllers are only called
   // when they are recreated or on app start, instead of every page change.
